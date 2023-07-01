a7ca78ab464a1dc471683d94e4a02c778daa20a1
jiaulislam <jiaulislam.ict.bd@gmail.com>
2023-07-01 22:14:06 +0600

    :recycle:  adding uvicorn run for main run

--- notify/__init__.py
+++ notify/__init__.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 
@@ -40,3 +41,7 @@ app.include_router(router.app_router, prefix=API_VERSION_V1)
 
 
 app.openapi = custom_openapi
+
+
+if __name__ == "__main__":
+    uvicorn.run("notify:app", host="127.0.0.1", port=8000)
